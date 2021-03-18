516b411e5a85b87eb0617c25d6e542c0f5405612
byaruhangabrian <byaruhangabrian@gmail.com>
2021-03-18 10:48:21 +0300

    cleanup code
    Signed-off-by: byaruhangabrian <byaruhangabrian@gmail.com>

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import yfinance as yf
 import streamlit as st
 import pandas as pd
-# from datetime import datetime
 
 st.write("""
 # Simple Stock Price App
@@ -9,10 +8,12 @@ st.write("""
 
 #  2017 Ticker list from https://investexcel.net/all-yahoo-finance-stock-tickers/
 
+
 @st.cache
 def get_data():
     path = r'Yahoo Ticker Symbols - September 2017.csv'
     return pd.read_csv(path)
+
 
 tickerList = get_data()
 tickers = tickerList["Ticker"].drop_duplicates()
@@ -23,9 +24,7 @@ start_date = st.sidebar.date_input('Start Date')
 end_date = st.sidebar.date_input('End Date')
 
 # https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
-# define the ticker symbol
-# tickerSymbol = 'BTC-USD'
-# get data on this ticker
+
 tickerData = yf.Ticker(tickerSymbol)
 # get the historical prices for this ticker
 tickerDf = tickerData.history(period='1d', start=start_date, end=end_date)
